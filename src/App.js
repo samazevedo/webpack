ed20adfcd13f14993e0c4647630a975b6b299311
Sam Azevedo <samazevedo@Sams-MacBook-Air.local>
2022-06-03 16:01:14 -0300

    new branch

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-    return 'Webpack App'
-}
-
-export default App
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import App from './App'
-import './styles/main.scss'
-import './assets/emoji.png'
-console.log(App)
